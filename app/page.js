860909b1b761832421f809adfcb8c61aedae331c
JasperMunene <jaspermunene2022@gmail.com>
2024-04-20 17:45:06 +0300

    footer

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Slider from "./_components/Slider";
 import GlobalApi from "./_utils/GlobalApi";
 import CategoryList from "./_components/CategoryList";
 import ProductList from "./_components/ProductList";
+import Footer from "./_components/Footer";
 
 export default async function Home() {
 
@@ -23,6 +24,18 @@ return (
 
    {/* Product List */}
    <ProductList productList={productList} />
+
+   {/* Banner */}
+   <Image 
+    src='/banner.png'
+    width={1000}
+    height={300}
+    alt="banner"
+    className="w-full h-[400px] object-contain"
+   />
+  
+  {/* Footer */}
+  <Footer />
    </div>
   );
 }
